c4f9f95c5dcb8d684d33807a39be3d49bc8ca6d9
MrLuisKiller <luis_bol_1998@hotmail.com>
2026-04-07 11:40:01 -0600

    07/04/2026 - MiniFix Genero

--- controllers/distReportes.controller.js
+++ controllers/distReportes.controller.js
@@ -35,7 +35,7 @@ export const ListaFormulas = async (req = request, res = response) => {
     const { id_distrito } = req.data;
     const { clave_colonia } = req.body;
     try {
-        const datos = (await SICOVACC.sequelize.query(`SELECT secuencial, UPPER(CONCAT(nombre, ' ', paterno, ' ', materno)) AS nombre, edad, CASE genero WHEN 'F' THEN 'FEMENINO' WHEN 'M' THEN 'MASCULINO' ELSE NULL END AS genero, UPPER(cargo) AS cargo, folio
+        const datos = (await SICOVACC.sequelize.query(`SELECT secuencial, UPPER(CONCAT(nombre, ' ', paterno, ' ', materno)) AS nombre, edad, CASE genero WHEN 'm' THEN 'MUJER' WHEN 'h' THEN 'HOMBRE' ELSE NULL END AS genero, UPPER(cargo) AS cargo, folio
         FROM copaco_formulas
         WHERE secuencial IS NOT NULL AND id_distrito = ${id_distrito} AND clave_colonia = '${clave_colonia}'
         ORDER BY LEN(secuencial), secuencial ASC`))[0];
